fca8c6ead414f0b20ffa2f8d10920cc981674367
Starkoans <starkoans@gmail.com>
2024-08-30 12:09:24 +0500

    feat: выполнила задание

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) n = n * -1;
+    let digits = String(n).split("");
+    return Number(digits.reverse().join(""));
+};
